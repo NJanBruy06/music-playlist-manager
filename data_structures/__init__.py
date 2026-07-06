f85ab7b94abc9fc4711cac61238683558df67d21
NJanBruy06 <NJanBruy06@users.noreply.github.com>
2026-07-06 17:40:45 +0700

    Remove data_structures code

--- data_structures/__init__.py
+++
@@ -1,4 +0,0 @@
-from data_structures.node import Node
-from data_structures.doubly_linked_list import DoublyLinkedList
-
-__all__ = ["Node", "DoublyLinkedList"]
--- data_structures/node.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    def __init__(self, data):
-        self.data = data  
-        self.prev = None
-        self.next = None
-
-    def __repr__(self):
-        return f"Node({self.data!r})"
